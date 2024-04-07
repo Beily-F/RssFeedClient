45275cd4bb45e793142e6838c8e7538f21a5b6ef
heny3278 <heny3278@gmail.com>
2024-04-07 09:48:37 +0300

    third

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import {MatSelectModule} from '@angular/material/select';
+// import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations' ;
+import { FormsModule } from '@angular/forms';
+// import {MatIconModule} from '@angular/material/icon';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatTableModule} from '@angular/material/table';
+import { ViewComponent } from './view/view.component';
+// import {MatDialogModule} from '@angular/material/dialog';
+// import {
+//   MatDialog,
+//   MatDialogRef,
+//   MatDialogActions,
+//   MatDialogClose,
+//   MatDialogTitle,
+//   MatDialogContent,
+// } from '@angular/material/dialog';
+// import {MatButtonModule} from '@angular/material/button';
+
+
+
+
+// ... other Material module imports
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // MatSelectModule,
+    // HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule ,
+    // MatFormFieldModule,
+    // MatInputModule,
+    // MatTableModule,
+    // MatDialogModule,
+    // MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
